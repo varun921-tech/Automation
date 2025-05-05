61d6eb5548344f94276a865d84eb99ed5b7866ef
Varun Garg <varun_garg@epam.comsdfsdfs>
2025-05-05 14:10:38 +0530

    Made some changes

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -5,12 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import java.lang.reflect.Method;
+import java.util.*;
+
 public class BasePage {
+    public static void main(String[] args) {
+    }
 
     WebDriver driver;
     public BasePage(WebDriver driver){
         this.driver = driver;
-        driver = DriverManager.getDriver("CHROME");
         PageFactory.initElements(driver, this);
     }
+
 }
--- target/test-classes/features/login.feature
+++
@@ -1,8 +0,0 @@
-Feature: Login Page Feature
-
-  Scenario: Successful Login
-    Given user is on the login page
-    When user enters the "Admin" in the username field
-    And user enters the password "admin123" in the password field
-    When user clicks on the login button
-    Then user lands on the dashboard page
